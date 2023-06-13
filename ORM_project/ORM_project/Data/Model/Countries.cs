7af6a964ab8a405bba8143360cf730ce268d3ed0
Pana_yc94@itstep.edu.az <Pana_yc94@itstep.edu.az>
2023-06-13 14:04:24 +0400

    first

---
+++ ORM_project/ORM_project/Data/Model/Countries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ORM_project.Model
+{
+    public class Countries
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime FoundationDate { get; set; }
+
+        [Required]
+        public string GovernmentType { get; set; }
+
+        [Required]
+        public string MapLink { get; set; }
+
+        [Required]
+        public int Population { get; set; }
+
+        [Required]
+        public int Area { get; set; }
+
+        [Required]
+        public int GDP { get; set; }
+
+    }
+}
