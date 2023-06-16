6f8c6ce75677780fab4b025cd53d4b743da178be
Pana_yc94@itstep.edu.az <Pana_yc94@itstep.edu.az>
2023-06-16 10:02:34 +0400

    first

---
+++ ORM/ORM/Data/Model/Country.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Data.Model
+{
+    public class Country
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime FoundationDate { get; set; }
+
+        [Required]
+        public string GovernmentType { get; set; }
+
+        [Required]
+        public string MapLink { get; set; }
+
+        [Required]
+        public uint Population { get; set; }
+
+        [Required]
+        public uint Area { get; set; }
+
+        [Required]
+        public uint GDP { get; set; }
+
+    }
+}
---
+++ ORM/ORM/View/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ORM.Data.Model;
+using ORM.Data.Contexts;
+using System.Data.SqlClient;
+
+
+namespace ORM
+{    public partial class MainWindow : Window
+    {
+        public List<Country> MyCountries { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (CountryDbContext _context = new CountryDbContext())
+            {
+                MyCountries =  _context.Countries.ToList();
+            }
+
+            CountriesList.ItemsSource = MyCountries;
+        }
+    }
+}
