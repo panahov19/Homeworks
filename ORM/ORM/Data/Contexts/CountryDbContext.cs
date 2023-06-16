74c9e56fa172918d389aa65573b2cbdbcf614ed1
Pana_yc94@itstep.edu.az <Pana_yc94@itstep.edu.az>
2023-06-16 11:25:09 +0400

    project added

---
+++ ORM/ORM/Data/Contexts/CountryDbContext.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using ORM.Data.Model;
+using System.Text.Json;
+using System.Data.SqlClient;
+
+namespace ORM.Data.Contexts
+{
+    public class CountryDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        public CountryDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-CPU5UM7;Initial Catalog=Countries;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+           modelBuilder.Entity<Country>().HasNoKey();
+        }
+
+    }
+}
---
+++ ORM/ORM/Migrations/20230616070958_President and Hymn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class PresidentandHymn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Hymn",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "President",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hymn",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "President",
+                table: "Countries");
+        }
+    }
+}
